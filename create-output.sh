bba2d9959d6ba0f6c17ad15aa9d1e96f91f7aaaa
Jakub Partyka <kontakt@jakub-partyka.pl>
2020-08-16 19:12:39 +0200

    now using .tmp file

--- create-output.sh
+++ create-output.sh
@@ -1,6 +1,6 @@
-root@vps624097:~/monitoring# cat create-output.sh 
 #!/bin/bash
 OUTPUT=/var/www/html/index.html
+TMP=/tmp/monitoring.tmp
 LOG=/var/mon.log
 echo "<html>
   <head>
@@ -11,22 +11,22 @@ echo "<html>
 
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
-	['Time', 'Memory usage (%)', 'CPU usage (%)']," > $OUTPUT;
+        ['Time', 'Memory usage (%)', 'CPU usage (%)']," > $TMP
 
 IFS=$'\n'
 for line in $(cat $LOG) ; do
-	val=$(echo $line | cut --delimiter=' ' -f 1)
-	cpu=$(echo $line | cut --delimiter=' ' -f 2)
-	d1=$(echo $line | cut --delimiter=' ' -f 3)
-	d2=$(echo $line | cut --delimiter=' ' -f 4)
-	val=$(bc <<< "scale=2; $val/100")
-	echo "['$d1 $d2',$val,$cpu]," >> $OUTPUT
+        val=$(echo $line | cut --delimiter=' ' -f 1)
+        cpu=$(echo $line | cut --delimiter=' ' -f 2)
+        d1=$(echo $line | cut --delimiter=' ' -f 3)
+        d2=$(echo $line | cut --delimiter=' ' -f 4)
+        val=$(bc <<< "scale=2; $val/100")
+        echo "['$d1 $d2',$val,$cpu]," >> $TMP
 done
 
 echo "        ]);
 
         var options = {
-          title: 'Memory/CPU usage',
+          title: 'Memory usage',
           hAxis: {title: 'Time',  titleTextStyle: {color: '#333'}},
           vAxis: {minValue: 0, maxValue: 100}
         };
@@ -39,4 +39,7 @@ echo "        ]);
   <body>
     <div id=\"chart_div\" style=\"width: 100%; height: 100%;\"></div>
   </body>
-</html>" >> $OUTPUT
+</html>" >> $TMP
+
+#write contents of .tmp file to output file
+mv $TMP $OUTPUT
