42cd921beca8e890c3f43841d241dfbe72b55833
Jakub Partyka <kontakt@jakub-partyka.pl>
2020-08-18 16:18:19 +0200

    fixed log clearing bug on reaching max lines count

--- logger.sh
+++ logger.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-max_lines=1000
+max_lines=20
 output="/var/mon.log"
-memfree=`cat /proc/meminfo | grep MemAvailable | awk '{print $2}'`; 
+tmplog="/tmp/mon.log.tmp"
+memfree=`cat /proc/meminfo | grep MemAvailable | awk '{print $2}'`;
 memtotal=`cat /proc/meminfo | grep MemTotal | awk '{print $2}'`;
 memused=$((memtotal-memfree))
 memused=$((memused*10000/memtotal))
@@ -11,12 +12,12 @@ cpuused=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {prin
 lines=$(wc -l $output | cut --delimiter=' ' -f1)
 
 #cut first line if max number was exceeded (memory)
-if [[ $lines -gt $max_lines ]] ; then
-	tail -n +2 "$output" > "$output"
+if [[ $lines -ge $max_lines ]] ; then
+        tail -n +2 "$output" > "$tmplog" && mv "$tmplog" "$output"
 fi
 
 #log used memory to file
 echo "$memused $cpuused $(date '+%d.%m.%y %H:%M')" >> $output
 
-#update html file by calling create-output script
-./create-output.sh
+#update html file
+#/root/monitoring/create-output.sh
