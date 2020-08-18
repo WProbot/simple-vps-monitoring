2d9404913b23176702a81fcb1242f875c7bf0ba1
Jakub Partyka <kontakt@jakub-partyka.pl>
2020-08-18 16:40:40 +0200

    simplified mem used calculation step

--- logger.sh
+++ logger.sh
@@ -2,10 +2,7 @@
 max_lines=20
 output="/var/mon.log"
 tmplog="/tmp/mon.log.tmp"
-memfree=`cat /proc/meminfo | grep MemAvailable | awk '{print $2}'`;
-memtotal=`cat /proc/meminfo | grep MemTotal | awk '{print $2}'`;
-memused=$((memtotal-memfree))
-memused=$((memused*10000/memtotal))
+memused=$( free |  awk '$1 == "Mem:" {x= $7/($3+$7)*100} $2 == "buffers/cache:" {x= $4/($3+$4)*100} $1 == "Swap:" {print (100-x)}')
 cpuused=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
 
 #current number of lines in output file
@@ -13,11 +10,13 @@ lines=$(wc -l $output | cut --delimiter=' ' -f1)
 
 #cut first line if max number was exceeded (memory)
 if [[ $lines -ge $max_lines ]] ; then
-        tail -n +2 "$output" > "$tmplog" && mv "$tmplog" "$output"
+	tail -n +2 "$output" > "$tmplog" && mv "$tmplog" "$output"
 fi
 
-#log used memory to file
-echo "$memused $cpuused $(date '+%d.%m.%y %H:%M')" >> $output
+
+#log used memory & cpu to file
+timestamp=$(date '+%d.%m.%y %H:%M')
+echo "['$timestamp',$memused,$cpuused]," >> "$output"
 
 #update html file
-#/root/monitoring/create-output.sh
+/root/monitoring/create-output.sh $output
